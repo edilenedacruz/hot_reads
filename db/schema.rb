7babb093588250e56510ec056723096d6eab13f3
Edilene <edilene_cruz@yahoo.com.br>
2017-06-10 21:12:12 -0600

    this merge doesn't have the timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,10 @@ ActiveRecord::Schema.define(version: 20170605213229) do
   enable_extension "plpgsql"
 
   create_table "links", force: :cascade do |t|
-    t.string  "url"
-    t.integer "count", default: 0
+    t.string   "url"
+    t.integer  "count",      default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
