91009a353bf595350e136c051dfaa89a0958e182
Edilene <edilene_cruz@yahoo.com.br>
2017-06-10 21:07:26 -0600

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+link_1 = Link.create(url: "http://www.google.com")
+link_2 = Link.create(url: "https://www.turing.io")
+link_3 = Link.create(url: "http://www.amazon.com")
