d1d440e01de280823627905e277a4eb23b9c1086
Edilene <edilene_cruz@yahoo.com.br>
2017-06-16 10:08:12 -0600

    final changes

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -9,6 +9,7 @@ class Api::V1::LinksController < ApplicationController
     @link = Link.find_or_create_by(url: link_params[:url])
     @link.count += 1
     @link.save
+    render json: Link.where('updated_at > ?', 24.hours.ago).order(count: :desc).limit(10) 
   end
 
   private
